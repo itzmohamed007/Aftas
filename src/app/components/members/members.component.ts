4439b2e37f1ccad9e1550374d7b3dd1185b30912
= <itzmohamed007@gmail.com>
2023-12-18 05:57:03 +0100

    added members table

--- src/app/components/members/members.component.ts
+++ src/app/components/members/members.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {MemberService} from "../../services/member.service";
+import {Member} from "../../models/Member";
 
 @Component({
   selector: 'app-members',
   templateUrl: './members.component.html',
   styleUrls: ['./members.component.css']
 })
-export class MembersComponent {
+export class MembersComponent implements OnInit {
+  members:Member[] = [];
+  constructor(private service:MemberService) {}
+  ngOnInit(): void {
+    this.loadMembers()
+  }
+  loadMembers() {
+    this.service.getMembers().subscribe({
+      next: members => {
+        this.members = members;
+        console.log(members)
+      },
+      error: err => {
+        console.log("an error occurred while fetching members");
+        console.log(err)
+      }
+    })
+  }
 
 }
